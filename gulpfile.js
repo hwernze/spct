9dbca6427e22cef7b164a2a0c810cea5ba2a9863
hwernze <hwernze@gmail.com>
2016-06-14 10:33:17 +0200

    Browser-sync, gulp-watch and batch added BETA

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,10 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     concat = require('gulp-concat'),
     htmlmin = require('gulp-htmlmin'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    watch = require('gulp-watch'),
+    batch = require('gulp-batch'),
     browserSync = require('browser-sync').create();
 
 var param = process.argv[process.argv.length -1];
@@ -19,7 +23,7 @@ var param = process.argv[process.argv.length -1];
 //----------//
 // SETTINGS //
 //----------//
-//TODO: refern always to min css/js
+//TODO: always compile to min css/js
 
 var config = {
     html_src: './src/*.html',
@@ -33,14 +37,15 @@ var config = {
 	js_file_min: 'app.min.js',
     js_mangle: param === '--build' ? true : false,
     js_compress: true,
-    output_dest: param === '--build' ? './build' : './preprod'
+    output_dest: param === '--build' ? './build' : './preprod',
+    asset_srcImages: './src/img/**/*'
 }
 
 //-------//
 // TASKS //
 //-------//
 
-//SCSS
+// SCSS
 gulp.task('scss:compile', function () {
     return gulp.src(config.scss_srcFolder + config.scss_files)
     .pipe(sourcemaps.init())
@@ -55,7 +60,7 @@ gulp.task('css:rename', ['scss:compile'], function() {
     if (param === '--build' || param === '--pre') {
         gulp.src(config.output_dest + '/css/' + config.css_file)
         // gulp.src(output.css_file)
-        .pipe(rename(config.css_file_min))
+        .pipe(rename(config.css_file_min)) //{ suffix: '.min' }
         .pipe(gulp.dest(config.output_dest + '/css'));
     }
 });
@@ -66,12 +71,7 @@ gulp.task('css:cleanFolder', ['css:rename'], function() {
     };
 });
 
-// gulp.task('copyCSS', function() {
-//    gulp.src(src.css_folder)
-//    .pipe(gulp.dest(output.css_folder));
-// });
-
-//JAVASCRIPT
+// JAVASCRIPT
 gulp.task('scripts:concat', function() {
     return gulp.src([config.js_srcFolder + config.js_files, '!src/js/**/*.min.js'])
     .pipe(concat(config.js_file_min))
@@ -98,32 +98,78 @@ gulp.task('html:minify', function() {
   return gulp.src(config.html_src)
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest(config.output_dest))
+    .pipe(browserSync.stream());
 });
 gulp.task('html-watch', ['html:minify'], browserSync.reload);
+// ASSETS IMAGES
+gulp.task('assets:imagemin', function () {
+    return gulp.src(config.asset_srcImages)
+        .pipe(imagemin({
+            progressive: true,
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest(config.output_dest + '/img'));
+});
 
+gulp.task('asset-watch', ['assets:imagemin'], browserSync.reload);
 
 //----------//
 // COMMANDS //
 //----------//
+
+//UNBIND EXAMPLE
+/*
+var callback = function(stream) {
+  console.log('someone connected!');
+};
+server.on('connection', callback);
+// ...
+server.removeListener('connection', callback);
+*/
+
 // Static Server + watching scss/html files
-gulp.task('server', ['scss:compile'], function() {
+gulp.task('serve', [ 'build'], function() {
 
     browserSync.init({
         server: "./preprod"
     });
 
+    function scssCallback() {
+        gulp.start('scss:compile');
+        // gulp.watch(config.scss_srcFolder + config.scss_files, ['scss:compile'])
+    };
+
+    watch(config.scss_srcFolder + config.scss_files, scssCallback );
     gulp.watch(config.scss_srcFolder + config.scss_files, ['scss:compile']);
-    gulp.watch(config.js_srcFolder + config.js_files, ['js-watch']);
-    gulp.watch(config.html_src, ['html-watch']).on('change', browserSync.reload);
+
+    //Watch the scss folder for changes
+    // watch(config.scss_srcFolder + config.scss_files, batch(function (events, done){
+    //     gulp.start('scss:compile',
+    //         //callback when done
+    //         gulp.watch(config.scss_srcFolder + config.scss_files, ['scss:compile'])
+    //     );
+    // }));
+
+    // watch the js folder for changes
+    watch(config.js_srcFolder + config.js_files, batch(function (events, done) {
+        gulp.start('scripts:concat', 'scripts:compress',
+            //callback when done
+            gulp.watch(config.js_srcFolder + config.js_files, ['js-watch'])
+        );
+    })).on('change', browserSync.reload);
+
+
+    watch(config.asset_srcImages, batch(function (events, done) {
+        //TODO clean folder
+        gulp.start('assets:imagemin', done);
+    })).on('change', browserSync.reload);
+
+    gulp.watch(config.html_src, ['html-watch']);
+    // gulp.watch(config.html_src, ['html-watch']).on('change', browserSync.reload);
+    // gulp.watch(config.html_src, ['html:minify']);
 });
 
 
-gulp.task('watch', function () {
-    gulp.watch(config.scss_srcFolder + config.scss_files, ['scss:compile']);
-    gulp.watch(config.js_srcFolder + config.js_files, ['scripts:concat', 'scripts:compress']);
-    gulp.watch(config.html_src, ['html:minify']);
-});
+gulp.task('default', ['scss:compile', 'scripts:concat', 'scripts:compress', 'html:minify', 'assets:imagemin']);
 
-gulp.task('default', ['scss:compile', 'scripts:concat', 'scripts:compress', 'html:minify']);
-
-gulp.task('build',['scss:compile', 'css:rename', 'css:cleanFolder', 'scripts:concat', 'scripts:compress', 'html:minify']);
+gulp.task('build',['scss:compile', 'css:rename', 'css:cleanFolder', 'scripts:concat', 'scripts:compress', 'html:minify', 'assets:imagemin']);
---
+++ src/js/app.js
@@ -0,0 +1,1 @@
+var app_js = "some value string";
